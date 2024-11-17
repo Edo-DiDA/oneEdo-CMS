db417d6b21baac846b66cd5a77becdbf222b431f
EthicalRalph <ralph@darey.io>
2024-11-17 11:14:51 +0100

    chore: updates

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        region: env("AWS_REGION", "af-south-1"),
+        params: {
+          ACL: env("AWS_ACL", "public-read"),
+          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 6000),
+          Bucket: env("AWS_BUCKET", "pdoprototypes-edo-prototype-storage1"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
